277823dca8bdd067e401e2bc4239c610ddbc487c
Neri Barnini <admin@queeply.com>
2014-03-16 16:34:20 +0100

    Update jquery.imagecover.js

--- js/jquery.imagecover.js
+++ js/jquery.imagecover.js
@@ -14,187 +14,187 @@
 
 (function($) {
 
-  	$.fn.imagecover = function(options) {
-  	
-  		var $container 	= this,
-		    defaults 	=  {
-		    	throttle		: 200, 
-		    	runOnce			: false,
-		    	css2			: false,
-		    	preloadAllImages: false,
-		    	addPreload		: false,
-		    	loadingClass	: 'ic-loading'
-		    },
-	        settings = $.extend({}, defaults, options);
+    $.fn.imagecover = function(options) {
+    
+        var $container  = this,
+            defaults    =  {
+                throttle        : 200, 
+                runOnce         : false,
+                css2            : false,
+                preloadAllImages: false,
+                addPreload      : false,
+                loadingClass    : 'ic-loading'
+            },
+            settings = $.extend({}, defaults, options);
 
         var prepareCoverImage = function() {
-        	
-        	$container.each(function() {
-				var $single		 = $(this),
-	        		$img 		 = $single.find('>img').first().css({'position':'absolute'}),
-	        		containerPos = $single.css('position');
-				
-	    	    $single.css({
-	    	    	'overflow':'hidden',
-	    	     	'position': (containerPos === 'static') ? 'relative' : containerPos
-	    	    });
+            
+            $container.each(function() {
+                var $single      = $(this),
+                    $img         = $single.find('>img').first().css({'position':'absolute'}),
+                    containerPos = $single.css('position');
+                
+                $single.css({
+                    'overflow':'hidden',
+                    'position': (containerPos === 'static') ? 'relative' : containerPos
+                });
 
-	    	    toggleLoading.call(this);
-	    	     
-	    	    var $preloadItem = (settings.preloadAllImages) ? $(this) : $img;
-	    	     
-			    $preloadItem.imagesLoaded().done(function (img) {
-			     	
-			    	var imgRatio;
-			     	
-			     	$single.data('ic-height',$single.height());
-			     	$single.data('ic-width', $single.width());
+                toggleLoading.call(this);
+                 
+                var $preloadItem = (settings.preloadAllImages) ? $(this) : $img;
+                 
+                $preloadItem.imagesLoaded().done(function (img) {
+                    
+                    var imgRatio;
+                    
+                    $single.data('ic-height',$single.height());
+                    $single.data('ic-width', $single.width());
 
-			     	var width 	= $img.width(),
-			     		height 	= $img.height();
+                    var width   = $img.width(),
+                        height  = $img.height();
 
-		      		imgRatio = (!width && !height) ? false : $img.width() / $img.height();
-		      		
-		      		$single.data('ic-imgRatio', imgRatio);
-			        
-			        if (!imgRatio) {
-			       	 	delegateCover.call(this);
-				    } else {
-				    	toggleLoading.call(this);
-				    	coverImage.call(this);
-				    }
-			    });
-	        	
-	        });
-	        
-	        if (!settings.runOnce)
-	        	window.setInterval(function() {
-	        		checkSizeChange()
-	        	}, settings.throttle);
+                    imgRatio = (!width && !height) ? false : $img.width() / $img.height();
+                    
+                    $single.data('ic-imgRatio', imgRatio);
+                    
+                    if (!imgRatio) {
+                        delegateCover.call(this);
+                    } else {
+                        toggleLoading.call(this);
+                        coverImage.call(this);
+                    }
+                });
+                
+            });
+            
+            if (!settings.runOnce)
+                window.setInterval(function() {
+                    checkSizeChange()
+                }, settings.throttle);
         };
         
 
         var toggleLoading = function() {
-        	var loaded = $(this).hasClass(settings.loadingClass);
+            var loaded = $(this).hasClass(settings.loadingClass);
 
-        	if (loaded) {
-        		$(this).removeClass(settings.loadingClass);        		
-        	} else {
-        		$(this).addClass(settings.loadingClass);        		
-        	}
+            if (loaded) {
+                $(this).removeClass(settings.loadingClass);             
+            } else {
+                $(this).addClass(settings.loadingClass);                
+            }
 
-        	if (settings.addPreload) {
-        		var preloaderClass = (!settings.addPreload) ? settings.addPreload : 'ic-preloader';
+            if (settings.addPreload) {
+                var preloaderClass = (!settings.addPreload) ? settings.addPreload : 'ic-preloader';
 
-        		if (loaded) {
-        			var html = '<div class="' + preloaderClass + '" style="position:absolute; left:0; right:0; bottom:0; top:0; z-index:999999;"></div>';
-	        		$(this).prepend(html);
-        		} else {
-        			$(this).find('.' + preloaderClass).fadeOut(function() {
-        				$(this).remove();
-        			});
-        		}
-        	}
+                if (loaded) {
+                    var html = '<div class="' + preloaderClass + '" style="position:absolute; left:0; right:0; bottom:0; top:0; z-index:999999;"></div>';
+                    $(this).prepend(html);
+                } else {
+                    $(this).find('.' + preloaderClass).fadeOut(function() {
+                        $(this).remove();
+                    });
+                }
+            }
         };
 
         
         var coverImage = function(forced) {
 
-        	var $containerToCover = (forced) ? forced : $container;
-        	
-        	$containerToCover.each(function() {
-        		
-        		var $single	= $(this),
-        			$img 	= $single.find('>img').first();
-        			width 	= $single.width(),
-        			height 	= $single.height(),
-        			iRatio 	= $single.data('ic-imgRatio'),
-        			cRatio 	= width / height;
-				
-		        if (typeof iRatio === "undefined" || !iRatio) 
-		        	return false;
+            var $containerToCover = (forced) ? forced : $container;
+            
+            $containerToCover.each(function() {
+                
+                var $single = $(this),
+                    $img    = $single.find('>img').first();
+                    width   = $single.width(),
+                    height  = $single.height(),
+                    iRatio  = $single.data('ic-imgRatio'),
+                    cRatio  = width / height;
+                
+                if (typeof iRatio === "undefined" || !iRatio) 
+                    return false;
 
-		        if (cRatio < iRatio) {
-		          	$img.css({
-		            	width: 'auto',
-		            	height: height,
-		            	top: 0,
-		            	left: -((height * iRatio) - width) / 2
-		            });
-				} else {
-		          	$img.css({
-		              	width: width,
-		              	height: 'auto',
-		              	top: -((width / iRatio) - height) / 2,
-		              	left: 0
-		            });
-		        }
-        	});
+                if (cRatio < iRatio) {
+                    $img.css({
+                        width: 'auto',
+                        height: height,
+                        top: 0,
+                        left: -((height * iRatio) - width) / 2
+                    });
+                } else {
+                    $img.css({
+                        width: width,
+                        height: 'auto',
+                        top: -((width / iRatio) - height) / 2,
+                        left: 0
+                    });
+                }
+            });
         };
         
         var coverImageCss3 = function() {
-        	$container.each(function() {
-        		var $single = $(this),
-	        		$img 	= $single.find('>img').first();
+            $container.each(function() {
+                var $single = $(this),
+                    $img    = $single.find('>img').first();
         
-        		var $preloadItem = (settings.preloadAllImages) ? $single : $img;
+                var $preloadItem = (settings.preloadAllImages) ? $single : $img;
 
-        		toggleLoading.call(this);
-			    
-			    $preloadItem.imagesLoaded().done(function (img) {
-			    	toggleLoading.call(this);
-			    });
-        		
-	        	$single.css({
-	        		'background-repeat':'no-repeat',
-	        		'background-position':'center',
-	        		'background-size':'cover',
-	        		'background-image':'url(' + $img.attr('src') + ')'
-	        	});
+                toggleLoading.call(this);
+                
+                $preloadItem.imagesLoaded().done(function (img) {
+                    toggleLoading.call(this);
+                });
+                
+                $single.css({
+                    'background-repeat':'no-repeat',
+                    'background-position':'center',
+                    'background-size':'cover',
+                    'background-image':'url(' + $img.attr('src') + ')'
+                });
 
-	        	$img.remove();
-        	});
+                $img.remove();
+            });
         };
         
         var delegateCover = function() {
-        	var $single = $(this);
+            var $single = $(this);
 
-        	window.setTimeout(function(){
-				var $img = $single.find('>img').first();
-				
-				imgRatio = (!width && !height) ? false : $img.width() / $img.height();
-		      		
-		      	$single.data('ic-imgRatio', imgRatio);
+            window.setTimeout(function(){
+                var $img = $single.find('>img').first();
+                
+                imgRatio = (!width && !height) ? false : $img.width() / $img.height();
+                    
+                $single.data('ic-imgRatio', imgRatio);
 
-		        if (!imgRatio) {
-		       	 	delegateCover.call(this);
-			    } else {
-			    	toggleLoading.call(this);
-			    	coverImage.call(this);
-			    }
-			}, settings.throttle);
+                if (!imgRatio) {
+                    delegateCover.call(this);
+                } else {
+                    toggleLoading.call(this);
+                    coverImage.call(this);
+                }
+            }, settings.throttle);
         };
         
-        var checkSizeChange = function() {    	
-    	  	$container.each(function() {
-    	  		var $single = $(this),
-		        	height  = $single.height(),
-			        width   = $single.width();
-		        		    
-			    if (($single.data('ic-height') !== height || $single.data('ic-height') !== width)) {
-			    	$single.data('ic-height', height);
-			      	$single.data('ic-height', width);
+        var checkSizeChange = function() {      
+            $container.each(function() {
+                var $single = $(this),
+                    height  = $single.height(),
+                    width   = $single.width();
+                            
+                if (($single.data('ic-height') !== height || $single.data('ic-height') !== width)) {
+                    $single.data('ic-height', height);
+                    $single.data('ic-height', width);
 
-			        coverImage($single);
-			    }
-	      	});
+                    coverImage($single);
+                }
+            });
         };
         
         
         if (!Modernizr.backgroundsize || settings.css2) 
-        	prepareCoverImage();
-       	else 
-       		coverImageCss3();
+            prepareCoverImage();
+        else 
+            coverImageCss3();
         
         return this;
   };
